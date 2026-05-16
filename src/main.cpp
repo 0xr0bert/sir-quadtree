0e32eca18fa94db0579db214ce0e83fb9e6a7fb1
Robert Greener <2730998+0xr0bert@users.noreply.github.com>
2026-05-16 08:58:50 +0100

    Add sir_state

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "vec2.h"
+#include "sir_state.h"
 
 int main() {
   std::cout << "Hello World!" << std::endl;
---
+++ src/sir_state.h
@@ -0,0 +1,7 @@
+#pragma once
+
+enum class SirState {
+  SUSCEPTIBLE,
+  INFECTED,
+  RECOVERED
+};
